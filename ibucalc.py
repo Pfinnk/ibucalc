173e8f3ee9990d9ef236d939cb530e0cdd60d2d8
Peter Knowlton <86847160+Pfinnk@users.noreply.github.com>
2023-08-15 14:43:58 -0700

    Update ibucalc.py

--- ibucalc.py
+++ ibucalc.py
@@ -5,7 +5,7 @@ import math
 def ibuimp(mo, aap, vg, og, t):
     
     #mo = hop mass in ounces
-    #aa = alpha acid % of hop as percentage
+    #aa = alpha-acid% of hop as percentage
     #vg = volume of batch in gallons
     #og = original gravity of wort
     #t = boil time of hop addition in mins
@@ -15,7 +15,7 @@ def ibuimp(mo, aap, vg, og, t):
     vl = vg * 3.785412
     mmg = mo * 28349.5
     
-    #og and t are used to determine hop %utilization
+    #og and t are used to determine hop utilization%
     #bf = Bigness factor to account for wort gravity
     #btf = Boil Time factor to account for boil time
     #source: https://realbeer.com/hops/research.html
@@ -31,7 +31,7 @@ def ibuimp(mo, aap, vg, og, t):
 def ibumet(mg, aap, vl, og, t):
     
     #mg = hop mass in grams
-    #aa = %alpha acid of hop as percentage
+    #aa = alpha-acid% of hop as percentage
     #vl = volume of batch in liters
     #og = original gravity of wort
     #t = boil time of hop addition in mins
@@ -40,7 +40,7 @@ def ibumet(mg, aap, vl, og, t):
     aad = aap/100
     mmg = mg * 1000
     
-    #og and t are used to determine hop %utilization
+    #og and t are used to determine hop utilization%
     #source: https://realbeer.com/hops/research.html
     bf = 1.65 * 0.000125**(og - 1)
     btf = (1 - math.exp(-0.04 * t))/4.15
